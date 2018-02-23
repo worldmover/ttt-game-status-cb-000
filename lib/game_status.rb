537a3bd99233dc34a37767bcc019882e20235e3a
Christian Weiss <worldmover777@gmail.com>
2018-02-23 00:35:16 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,10 +8,10 @@ WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4
 
 def won?(board)
   WIN_COMBINATIONS.each do |win|
-    if board[win][0] == "X" && board[win][1] == "X" && board[win][2] == "X"
+    if board[win[0]] == "X" && board[win[1]] == "X" && board[win[2]] == "X"
       return win
     end
-    if board[win][0] == "O" && board[win][1] == "O" && board[win][2] == "O"
+    if board[win[0]] == "O" && board[win[1]] == "O" && board[win[2]] == "O"
       return win
     end
   end
